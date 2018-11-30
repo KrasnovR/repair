47d2d0a571eb6378bc474082122d83dd9c4ef4b6
KrasnovR <krasnovr17@gmail.com>
2018-11-30 02:25:53 +0200

    sleep

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Модальное окно
 $(document).ready(function () {
   $('#open_call').on('click', function() {
     $('.modal').toggleClass('open');
@@ -6,3 +7,24 @@ $(document).ready(function () {
     $('.modal').toggleClass('open');
   });
 });
+
+
+//Слайдер с фотками
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:30,
+  nav:true,
+  dots:false,
+  navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
